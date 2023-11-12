b2482252e2aa818826b54860b966ede4e42acacc
Gabriel Padilha Santos <gabrielpadilhasantos@gmail.com>
2023-11-12 00:21:43 -0300

    frontend added

---
+++ frontend/src/pages/ShowBook.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import BlackButton from "../components/BlackButton";
+
+export default function ShowBook() {
+    const [book, setBook] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
+
+    useEffect(() => {
+        setLoading(true)
+        axios.get(`http://localhost:8080/api/books/${id}`)
+        .then((response) => {
+            setBook(response.data);
+            setLoading(false);
+        }).catch((error) => {
+            console.log(error);
+            setLoading(false);
+        });
+    }, [])
+
+    return (
+        <div className="p-4">
+            <BlackButton/>
+            <h1 className="text-3xl my-4">Show Book</h1>
+            {loading ? (<Spinner/>) : (
+                <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Id</span>
+                        <span>{book.id}</span>
+                    </div>
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Title</span>
+                        <span>{book.title}</span>
+                    </div>
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Author</span>
+                        <span>{book.author}</span>
+                    </div>
+                    <div className="my-4">
+                        <span className="text-xl mr-4 text-gray-500">Publish Year</span>
+                        <span>{book.publishYear}</span>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
